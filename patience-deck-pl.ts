38078f197e483044b20de26f74d009078c20d02b
PiotrD <likot180@gmail.com>
2022-03-20 15:41:47 +0100

    Create patience-deck-pl.ts

---
+++ patience-deck-pl.ts
@@ -0,0 +1,363 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL" sourcelanguage="en">
+<context>
+    <name></name>
+    <message id="patience-bt-contributors">
+        <location filename="../qml/pages/about/AboutPage.qml" line="31"/>
+        <source>Contributors</source>
+        <extracomment>Button to open the list of Patience Deck contributors (not implemented yet)</extracomment>
+        <translation>Współtwórcy</translation>
+    </message>
+    <message id="patience-he-contributors">
+        <location filename="../qml/pages/about/AboutPage.qml" line="34"/>
+        <source>Contributors of Patience Deck</source>
+        <oldsource>Contributors</oldsource>
+        <extracomment>Page header for the list of Patience Deck contributors (not implemented yet)</extracomment>
+        <translation>Współtwórcy Patience Deck</translation>
+    </message>
+    <message id="patience-se-developers">
+        <location filename="../qml/pages/about/AboutPage.qml" line="37"/>
+        <source>Developers</source>
+        <extracomment>Section header for list of developers of Patience Deck (not implemented yet)</extracomment>
+        <translation>Deweloperzy</translation>
+    </message>
+    <message id="patience-se-translators">
+        <location filename="../qml/pages/about/AboutPage.qml" line="40"/>
+        <source>Translators</source>
+        <extracomment>Section header for list of translators of Patience Deck (not implemented yet)</extracomment>
+        <translation>Tłumacze</translation>
+    </message>
+    <message id="patience-he-about">
+        <location filename="../qml/pages/about/AboutPage.qml" line="54"/>
+        <source>About</source>
+        <translation>O programie</translation>
+    </message>
+    <message id="patience-de-patience_deck">
+        <location filename="../qml/pages/about/AboutPage.qml" line="57"/>
+        <source>Patience Deck %1</source>
+        <extracomment>Name of this application, %1 is version number</extracomment>
+        <translation>Patience Deck %1</translation>
+    </message>
+    <message id="patience-la-about_text">
+        <location filename="../qml/pages/about/AboutPage.qml" line="80"/>
+        <source>Patience Deck is a collection of %1 supported patience games for Sailfish OS. It reimplements game engine from &lt;a href=%2&gt;GNOME Aisleriot&lt;/a&gt; and utilises its implementations of patience games including translations, manual pages and artwork.</source>
+        <oldsource>Patience Deck is a collection of %1 supported patience games for Sailfish OS. It reimplements game engine from &lt;a href=%2&gt;GNOME Aisleriot&lt;/a&gt; and utilises its implementations of patience games including manual pages and artwork.</oldsource>
+        <translatorcomment>OS avattu käyttöjärjestelmäksi, koska sille ei ole hyvää lyhennystä suomeksi</translatorcomment>
+        <translation>Patience Deck to zbiór %1 gier typu Pasjans dla systemu Sailfish OS. Wykorzystuje silnik gier z &lt;a href=%2&gt;GNOME Aisleriot&lt;/a&gt; i wykorzystuje jego implementację pasjansów, w tym tłumaczenia, strony podręcznika i grafiki.</translation>
+    </message>
+    <message id="patience-se-development">
+        <location filename="../qml/pages/about/AboutPage.qml" line="89"/>
+        <source>Development</source>
+        <extracomment>Section to list developers and link to github</extracomment>
+        <translation>Rozwój</translation>
+    </message>
+    <message id="patience-la-see_also_manuals">
+        <location filename="../qml/pages/about/AboutPage.qml" line="94"/>
+        <source>Main developer: %1</source>
+        <translation>Główny deweloper: %1</translation>
+    </message>
+    <message id="patience-la-source_code_report_bugs_github">
+        <location filename="../qml/pages/about/AboutPage.qml" line="102"/>
+        <source>You may obtain source code and report bugs on Github: &lt;a href=%2&gt;%1&lt;/a&gt;</source>
+        <translation>Możesz uzyskać kod źródłowy i zgłosić błędy na Github: &lt;a href=%2&gt;%1&lt;/a&gt;</translation>
+    </message>
+    <message id="patience-se-acknowledgements">
+        <location filename="../qml/pages/about/AboutPage.qml" line="111"/>
+        <source>Acknowledgements</source>
+        <extracomment>Thank you section for developers of GNOME Aisleriot</extracomment>
+        <translation>Podziękowania</translation>
+    </message>
+    <message id="patience-la-thank_you_gnome_aisleriot_authors">
+        <location filename="../qml/pages/about/AboutPage.qml" line="117"/>
+        <source>Thank you to all GNOME Aisleriot authors for making such high quality games to enjoy! This project would not be possible without your work!</source>
+        <translation>Dziękujemy wszystkim autorom GNOME Aisleriot za tworzenie tak wysokiej jakości gier. Ten projekt nie byłby możliwy bez Waszej pracy!</translation>
+    </message>
+    <message id="patience-bt-authors">
+        <location filename="../qml/pages/about/AboutPage.qml" line="123"/>
+        <source>Authors</source>
+        <translation>Autorzy</translation>
+    </message>
+    <message id="patience-se-license">
+        <location filename="../qml/pages/about/AboutPage.qml" line="131"/>
+        <source>License</source>
+        <extracomment>Section that mentions license(s) of Patience Deck</extracomment>
+        <translation>Licencja</translation>
+    </message>
+    <message id="patience-la-free_software_and_redistribution">
+        <location filename="../qml/pages/about/AboutPage.qml" line="139"/>
+        <source>Patience Deck is free software, and you are welcome to redistribute it under certain conditions. This software comes with ABSOLUTELY NO WARRANTY. Tap the button below for more details.</source>
+        <translation>Patience Deck to darmowe oprogramowanie, które możesz rozpowszechniać pod pewnymi warunkami. To oprogramowanie nie jest objęte ABSOLUTNIE ŻADNĄ GWARANCJĄ. Kliknij przycisk poniżej, aby uzyskać więcej informacji.</translation>
+    </message>
+    <message id="patience-bt-licenses">
+        <location filename="../qml/pages/about/AboutPage.qml" line="145"/>
+        <source>Licenses</source>
+        <translation>Licencje</translation>
+    </message>
+    <message id="patience-se-experimental">
+        <location filename="../qml/pages/about/AboutPage.qml" line="153"/>
+        <source>Experimental</source>
+        <extracomment>Section for enabling experimental features</extracomment>
+        <translation>Funkcje eksperymentalne</translation>
+    </message>
+    <message id="patience-la-show_all_games">
+        <location filename="../qml/pages/about/AboutPage.qml" line="158"/>
+        <source>Show all games</source>
+        <translation>Pokaż wszystkie gry</translation>
+    </message>
+    <message id="patience-de-list_unsupported_games">
+        <location filename="../qml/pages/about/AboutPage.qml" line="160"/>
+        <source>List also unsupported games in game selection</source>
+        <translation>Wyświetl również nieobsługiwane gry w wyborze gier</translation>
+    </message>
+    <message id="patience-he-thanks_to">
+        <location filename="../qml/pages/about/AboutPage.qml" line="189"/>
+        <source>Thanks to</source>
+        <translation>Podziękowania dla</translation>
+    </message>
+    <message id="patience-de-aisleriot_authors">
+        <location filename="../qml/pages/about/AboutPage.qml" line="191"/>
+        <source>GNOME Aisleriot authors</source>
+        <translation>autorów GNOME Aisleriot</translation>
+    </message>
+    <message id="patience-se-translator-information">
+        <location filename="../qml/pages/about/AboutPage.qml" line="204"/>
+        <source>Aisleriot translator information for language</source>
+        <extracomment>Section to show Aisleriot translator/translation information, substitute word language with the name of the language of the translation. Original is pretty generic on purpose, feel free to translate this to something works for the result</extracomment>
+        <translation>Informacje o tłumaczu Aisleriot dla języka</translation>
+    </message>
+    <message id="patience-la-free_software_and_redistribution_multiple_licenses">
+        <location filename="../qml/pages/about/LicensesPage.qml" line="32"/>
+        <source>Patience Deck is free software, and you are welcome to redistribute it under certain conditions. This software comes with ABSOLUTELY NO WARRANTY. Patience Deck contains work licensed under multiple different licenses.</source>
+        <translation>Patience Deck to darmowe oprogramowanie, które możesz rozpowszechniać pod pewnymi warunkami. To oprogramowanie nie jest objęte ABSOLUTNIE ŻADNĄ GWARANCJĄ. Patience Deck zawiera kod źródłowy licencjonowany na podstawie wielu różnych licencji.</translation>
+    </message>
+    <message id="patience-la-harbour_distribution_bundles_libraries">
+        <location filename="../qml/pages/about/LicensesPage.qml" line="64"/>
+        <source>Harbour distribution of Patience Deck bundles several libraries that have their own licenses.</source>
+        <translation>Dystrybucja portu Patience Deck obejmuje kilka bibliotek, które mają własne licencje.</translation>
+    </message>
+    <message id="patience-he-licenses">
+        <location filename="../qml/pages/about/LicensesPage.qml" line="114"/>
+        <source>Licenses</source>
+        <translation>Licencje</translation>
+    </message>
+    <message id="patience-la-engine_failed">
+        <location filename="../qml/pages/components/EngineFailureOverlay.qml" line="33"/>
+        <source>Game engine failed</source>
+        <translation>Awaria silnika gry</translation>
+    </message>
+    <message id="patience-la-report_to_github">
+        <location filename="../qml/pages/components/EngineFailureOverlay.qml" line="46"/>
+        <source>Please report to Github:</source>
+        <translation>Proszę zgłosić przez Github:</translation>
+    </message>
+    <message id="patience-la-game_won">
+        <location filename="../qml/pages/components/GameOverOverlay.qml" line="68"/>
+        <source>You win</source>
+        <extracomment>Shown on top of the game when player has won the game</extracomment>
+        <translation>Wygrana</translation>
+    </message>
+    <message id="patience-la-game_over">
+        <location filename="../qml/pages/components/GameOverOverlay.qml" line="71"/>
+        <source>Game over</source>
+        <extracomment>Shown on top of the game when player has lost the game</extracomment>
+        <translation>Koniec gry</translation>
+    </message>
+    <message id="patience-bt-new_game">
+        <location filename="../qml/pages/components/GameOverOverlay.qml" line="95"/>
+        <location filename="../qml/pages/OptionsAndRules.qml" line="79"/>
+        <source>New game</source>
+        <extracomment>Starts a new game</extracomment>
+        <translation>Nowa gra</translation>
+    </message>
+    <message id="patience-bt-restart">
+        <location filename="../qml/pages/components/GameOverOverlay.qml" line="103"/>
+        <location filename="../qml/toolbar/Toolbar.qml" line="446"/>
+        <source>Restart</source>
+        <translation>Restart</translation>
+    </message>
+    <message id="patience-me-about">
+        <location filename="../qml/pages/Game.qml" line="101"/>
+        <source>About</source>
+        <translation>O programie</translation>
+    </message>
+    <message id="patience-me-select_game">
+        <location filename="../qml/pages/Game.qml" line="107"/>
+        <source>Select game</source>
+        <translation>Wybierz grę</translation>
+    </message>
+    <message id="patience-me-game_options">
+        <location filename="../qml/pages/Game.qml" line="114"/>
+        <source>Options &amp; Rules</source>
+        <translation>Opcje i Zasady</translation>
+    </message>
+    <message id="patience-me-common-settings">
+        <location filename="../qml/pages/OptionsAndRules.qml" line="42"/>
+        <source>Common settings</source>
+        <translation>Ogólne ustawienia</translation>
+    </message>
+    <message id="patience-he-options">
+        <location filename="../qml/pages/OptionsAndRules.qml" line="56"/>
+        <source>Options &amp; Rules</source>
+        <translation>Opcje i Zasady</translation>
+    </message>
+    <message id="patience-la-starting_new_or_changing_resuffle">
+        <location filename="../qml/pages/OptionsAndRules.qml" line="62"/>
+        <source>Starting a new game or changing game options will resuffle the cards</source>
+        <translation>Rozpoczęcie nowej gry lub zmiana opcji gry spowoduje ponowne rozdanie kart</translation>
+    </message>
+    <message id="patience-la-starting_new_resuffle">
+        <location filename="../qml/pages/OptionsAndRules.qml" line="64"/>
+        <source>Starting a new game will resuffle the cards</source>
+        <translation>Rozpoczęcie nowej gry spowoduje ponowne rozdanie kart</translation>
+    </message>
+    <message id="patience-se-game_options">
+        <location filename="../qml/pages/OptionsAndRules.qml" line="89"/>
+        <source>Options</source>
+        <translation>Opcje</translation>
+    </message>
+    <message id="patience-se-game_rules">
+        <location filename="../qml/pages/OptionsAndRules.qml" line="130"/>
+        <source>Rules</source>
+        <translation>Zasady</translation>
+    </message>
+    <message id="patience-he-games">
+        <location filename="../qml/pages/SelectGame.qml" line="32"/>
+        <source>Games</source>
+        <translation>Gry</translation>
+    </message>
+    <message id="patience-se-last_played">
+        <location filename="../qml/pages/SelectGame.qml" line="65"/>
+        <source>Last played</source>
+        <translation>Ostatnio grane</translation>
+    </message>
+    <message id="patience-se-favourites">
+        <location filename="../qml/pages/SelectGame.qml" line="68"/>
+        <source>Favourites</source>
+        <translation>Ulubione</translation>
+    </message>
+    <message id="patience-se-all_games">
+        <location filename="../qml/pages/SelectGame.qml" line="71"/>
+        <source>All games</source>
+        <translation>Wszystkie gry</translation>
+    </message>
+    <message id="patience-he-settings">
+        <location filename="../qml/pages/SettingsPage.qml" line="46"/>
+        <source>Settings</source>
+        <translation>Ustawienia</translation>
+    </message>
+    <message id="patience-la-affect-all-games">
+        <location filename="../qml/pages/SettingsPage.qml" line="51"/>
+        <source>These settings affect all games</source>
+        <translation>Te ustawienia wpływają na wszystkie gry</translation>
+    </message>
+    <message id="patience-se-appearance">
+        <location filename="../qml/pages/SettingsPage.qml" line="64"/>
+        <source>Appearance</source>
+        <translation>Wygląd</translation>
+    </message>
+    <message id="patience-la-background">
+        <location filename="../qml/pages/SettingsPage.qml" line="80"/>
+        <source>Background</source>
+        <translation>Tło</translation>
+    </message>
+    <message id="patience-me-solid">
+        <location filename="../qml/pages/SettingsPage.qml" line="99"/>
+        <source>Solid</source>
+        <extracomment>Solid (opaque) background color</extracomment>
+        <translation>Nieprzezroczyste</translation>
+    </message>
+    <message id="patience-me-adaptive">
+        <location filename="../qml/pages/SettingsPage.qml" line="105"/>
+        <source>Adaptive</source>
+        <extracomment>Background color is adapted to current ambience</extracomment>
+        <translation>Adaptacyjne</translation>
+    </message>
+    <message id="patience-me-transparent">
+        <location filename="../qml/pages/SettingsPage.qml" line="111"/>
+        <source>Transparent</source>
+        <extracomment>Transparent background, no color</extracomment>
+        <translation>Przezroczyste</translation>
+    </message>
+    <message id="patience-la-card_style">
+        <location filename="../qml/pages/SettingsPage.qml" line="168"/>
+        <source>Card style</source>
+        <extracomment>Combo box for selecting card style</extracomment>
+        <translation>Styl kard</translation>
+    </message>
+    <message id="patience-la-card_style_regular">
+        <location filename="../qml/pages/SettingsPage.qml" line="178"/>
+        <source>Regular</source>
+        <translation>Regularny</translation>
+    </message>
+    <message id="patience-la-card_style_optimised">
+        <location filename="../qml/pages/SettingsPage.qml" line="184"/>
+        <source>Optimised</source>
+        <extracomment>Optimised for mobile use</extracomment>
+        <translation>Optymalny</translation>
+    </message>
+    <message id="patience-la-card_style_simplified">
+        <location filename="../qml/pages/SettingsPage.qml" line="190"/>
+        <source>Simplified</source>
+        <extracomment>Simpler style, more suitable for tiny cards</extracomment>
+        <translation>Uproszczony</translation>
+    </message>
+    <message id="patience-se-gameplay">
+        <location filename="../qml/pages/SettingsPage.qml" line="200"/>
+        <source>Gameplay</source>
+        <translation>Rozgrywka</translation>
+    </message>
+    <message id="patience-la-prevent_display_blanking">
+        <location filename="../qml/pages/SettingsPage.qml" line="205"/>
+        <source>Prevent display from blanking</source>
+        <translation>Zapobiegaj wygaszeniu ekranu</translation>
+    </message>
+    <message id="patience-de-display_will_not_blank">
+        <location filename="../qml/pages/SettingsPage.qml" line="207"/>
+        <source>Display does not dim or turn black while a game is running</source>
+        <translation>Wyświetlacz nie przyciemnia się ani nie robi się czarny podczas gry</translation>
+    </message>
+    <message id="patience-la-play_feedback_effects">
+        <location filename="../qml/pages/SettingsPage.qml" line="214"/>
+        <source>Play feedback effects</source>
+        <translation>Odtwórz efekty wibracji</translation>
+    </message>
+    <message id="patience-de-play_feedback_effects">
+        <location filename="../qml/pages/SettingsPage.qml" line="216"/>
+        <source>Vibrates when dropping and clicking</source>
+        <translation>Wibruje podczas upuszczania kart i klikania</translation>
+    </message>
+    <message id="patience-bt-undo">
+        <location filename="../qml/toolbar/Toolbar.qml" line="397"/>
+        <source>Undo</source>
+        <translation>Cofnij</translation>
+    </message>
+    <message id="patience-bt-redo">
+        <location filename="../qml/toolbar/Toolbar.qml" line="409"/>
+        <source>Redo</source>
+        <translation>Powtórz</translation>
+    </message>
+    <message id="patience-bt-hint">
+        <location filename="../qml/toolbar/Toolbar.qml" line="421"/>
+        <source>Hint</source>
+        <translation>Wskazówka</translation>
+    </message>
+    <message id="patience-bt-deal">
+        <location filename="../qml/toolbar/Toolbar.qml" line="433"/>
+        <source>Deal</source>
+        <translation>Rozdaj</translation>
+    </message>
+    <message id="patience-la-score">
+        <location filename="../qml/toolbar/Toolbar.qml" line="495"/>
+        <location filename="../qml/toolbar/Toolbar.qml" line="557"/>
+        <source>Score:</source>
+        <translation>Wynik:</translation>
+    </message>
+    <message id="patience-la-time">
+        <location filename="../qml/toolbar/Toolbar.qml" line="520"/>
+        <location filename="../qml/toolbar/Toolbar.qml" line="570"/>
+        <source>Time:</source>
+        <translation>Czas:</translation>
+    </message>
+</context>
+</TS>
